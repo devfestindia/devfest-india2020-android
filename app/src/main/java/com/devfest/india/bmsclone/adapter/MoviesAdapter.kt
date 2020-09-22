77b002b7ebbe85130f9dc40b993b4ad721e424f4
niharika8059 <niharika.raj@1mg.com>
2020-09-22 12:11:58 +0530

    INTERMEDIATE Used Kotlin-Android extensions for XML views in place of findViewById()

--- app/src/main/java/com/devfest/india/bmsclone/adapter/MoviesAdapter.kt
+++ app/src/main/java/com/devfest/india/bmsclone/adapter/MoviesAdapter.kt
@@ -3,12 +3,11 @@ package com.devfest.india.bmsclone.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.devfest.india.bmsclone.R
 import com.devfest.india.bmsclone.model.Movie
+import kotlinx.android.synthetic.main.movie_item_layout.view.*
 
 /**
  * @author Niharika.Arora
@@ -31,23 +30,17 @@ class MoviesAdapter(private val movies: List<Movie>) :
     }
 
     class MoviesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val photo: ImageView = itemView.findViewById(R.id.movie_poster)
-        private val title: TextView = itemView.findViewById(R.id.movie_title)
-        private val releaseDate: TextView = itemView.findViewById(R.id.release_date)
-        private val averageVoting: TextView = itemView.findViewById(R.id.avg_voting)
-        private val totalVotes: TextView = itemView.findViewById(R.id.total_votes)
-
         companion object {
             private const val IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w500"
         }
 
         fun bind(movie: Movie) {
             Glide.with(itemView.context).load(IMAGE_BASE_URL + movie.posterPath)
-                .into(photo)
-            title.text = movie.title
-            releaseDate.text = movie.releaseDate
-            averageVoting.text = movie.voteAverage.toString()
-            totalVotes.text = movie.voteCount.toString()
+                .into(itemView.moviePoster)
+            itemView.movieTitle.text = movie.title
+            itemView.releaseDate.text = movie.releaseDate
+            itemView.avgVoting.text = movie.voteAverage.toString()
+            itemView.totalVotes.text = movie.voteCount.toString()
         }
     }
 }
